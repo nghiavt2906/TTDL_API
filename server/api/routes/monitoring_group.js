a252b9ce348aefc29a85d2e0e97eb7a2c4336afc
nghiavt2906 <nghiavt2906@gmail.com>
2022-03-23 10:50:46 +0700

    await for delete monitoring group

--- server/api/routes/monitoring_group.js
+++ server/api/routes/monitoring_group.js
@@ -3,7 +3,7 @@ import models from 'models'
 import * as func from "utils/functions"
 const router = Router()
 import bodyParser from 'body-parser'
-import {newId} from 'models/utils'
+import { newId } from 'models/utils'
 import app from 'app'
 
 router.use(bodyParser.urlencoded({ extended: false }));
@@ -14,7 +14,7 @@ export default expressRouter => {
 
 
   router.get("/", async (req, res, next) => {
-    const {monitoringType} = req.query 
+    const { monitoringType } = req.query
     let data = {}
     let monitoringTypeData = await app.MonitoringType.getMonitoringType()
     monitoringTypeData = func.changeToArrayFilter(monitoringTypeData, 'id', 'name')
@@ -27,25 +27,24 @@ export default expressRouter => {
 
   router.delete("/:managerId", async (req, res, next) => {
     try {
-      const {managerId} = req.params
+      const { managerId } = req.params
       await app.Manager.checkManagerPermission(managerId, 'delete_monitoring_group')
-      const {idMonitoringGroup} = req.query 
-      app.MonitoringGroup.deleteMonitoringGroup(idMonitoringGroup).then(result => {
-        res.sendStatus('200')
-      })
+      const { idMonitoringGroup } = req.query
+      await app.MonitoringGroup.deleteMonitoringGroup(idMonitoringGroup)
+      res.sendStatus('200')
     } catch (error) {
       console.log(error)
       next(error)
     }
-    
+
 
   })
 
   router.put("/:managerId", async (req, res, next) => {
     try {
-      const {managerId} = req.params
+      const { managerId } = req.params
       await app.Manager.checkManagerPermission(managerId, 'edit_monitoring_group')
-      const {idMonitoringGroup,infoMonitoringGroup} = req.body 
+      const { idMonitoringGroup, infoMonitoringGroup } = req.body
       // console.log({idMonitoringGroup,infoMonitoringGroup})
       app.MonitoringGroup.updateMonitoringGroup(idMonitoringGroup, infoMonitoringGroup).then(result => {
         res.sendStatus('200')
@@ -54,12 +53,12 @@ export default expressRouter => {
       console.log(error)
       next(error)
     }
-    
+
   })
 
   router.post("/:managerId", async (req, res, next) => {
     try {
-      const {managerId} = req.params
+      const { managerId } = req.params
       await app.Manager.checkManagerPermission(managerId, 'insert_monitoring_group')
       let id = newId()
       await app.MonitoringGroup.createMonitoringGroup(id, req.body)
