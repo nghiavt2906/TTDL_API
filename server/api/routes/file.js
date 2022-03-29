f9fd3cc4d2e536d4cfd5c2e9fdd7f6bc1ad5bc9d
nghiavt2906 <nghiavt2906@gmail.com>
2022-03-29 10:22:26 +0700

    add file path for tongquan

--- server/api/routes/file.js
+++ server/api/routes/file.js
@@ -8,9 +8,24 @@ const router = Router()
 
 export default expressRouter => {
   expressRouter.use("/file", router)
-  router.get("/:file", function(req, res, next) {
+  router.get("/:file", function (req, res, next) {
     try {
       req.dir = `${config.publicDir}/${req.params.file}`
+      // console.log(req.dir)
+      if (fs.existsSync(req.dir)) {
+        fs.createReadStream(req.dir).pipe(res)
+      } else {
+        res.end("File not found")
+      }
+    } catch (error) {
+      console.log(error)
+      next(error)
+    }
+  })
+
+  router.get("/tongquan/:file", function (req, res, next) {
+    try {
+      req.dir = `${config.publicDir}/tongquan/${req.params.file}`
       // console.log(req.dir)
       if (fs.existsSync(req.dir)) {
         fs.createReadStream(req.dir).pipe(res)
